c72f14c5c03124a47bf5c23637c5e1617b799ecb
Ahmed Hamdy <dimo.hamdy@gmail.com>
2014-07-21 10:27:06 +0200

    init
    init

---
+++ DHViewController.h
@@ -0,0 +1,13 @@
+//
+//  DHViewController.h
+//  DemoRemoveUnUsed
+//
+//  Created by binaryboy on 7/18/14.
+//  Copyright (c) 2014 AhmedHamdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHViewController : UIViewController
+
+@end
---
+++ DHViewController.m
@@ -0,0 +1,30 @@
+//
+//  DHViewController.m
+//  DemoRemoveUnUsed
+//
+//  Created by binaryboy on 7/18/14.
+//  Copyright (c) 2014 AhmedHamdy. All rights reserved.
+//
+
+#import "DHViewController.h"
+
+@interface DHViewController ()
+
+@end
+
+@implementation DHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    UIImageView *imageview=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"3.jpeg"]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
